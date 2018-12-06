97d33ff243223f4c9278bb95ef18fcf439910777
Dima <dima757@hotmail.com>
2018-12-05 21:22:48 -0600

    fixingendpoints

--- foodshareDB.sql
+++ foodshareDB.sql
@@ -10,27 +10,27 @@ CREATE TABLE users
 
     zip_code INTEGER(30),
 
-    password VARCHAR(30),
+    passcode VARCHAR(30),
 
-    user_id INTEGER(15)
+    user_id INTEGER(15),
 
     user_type CHARACTER
     (20)
 );
 
     Insert into users
-        (username, zip_code, password, user_id,user_type)
+        (username, zip_code, passcode, user_id,user_type)
     VALUES
         ("John", 75181, "password1", 1, "cook")
 
     Insert into users
-        (username, zip_code, password, user_id,user_type)
+        (username, zip_code, passcode, user_id,user_type)
     VALUES
         ("Billy", 75150, "password2", 2, "buyer")
 
     Insert into users
-        (username, zip_code, password, user_id,user_type)
+        (username, zip_code, passcode, user_id,user_type)
     VALUES
         ("Miguel", 75149, "password3", 3, "buyer")
 
-    UPDATE users
+    
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var mysql = require("mysql");
+// var inquirer = require("inquirer");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.post('/submit', (err, res) => {
-	console.log(req.body)
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Dima7735",
+  database: "foodshare_db"
 });
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
+// app.use('/static', express.static(path.join(__dirname, 'public')));
 
 
 app.use(bodyParser.json());
@@ -17,7 +31,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+app.post('/submit', (err, res) => {
+	console.log(req.body)
+});
 
+app.get('/getUsers', (err, res) => {
+  console.log('hit endpoint')
+  var data = connection.query('SELECT * FROM users', function (err, res) {
+    if (err) throw err;
+
+    for (i = 0; i < res.length; i++) {
+        console.log('username' + res[i].username + ' zip_code ' + res[i].zip_code + 'passcode ' + res[i].passcode + 'user_id ' + res[i].user_id + 'user_type ' + res[i].type )
+    }
+    console.log(JSON.stringify(res), 'response')
+})
+console.log(data, 'data')
+res.send(JSON.stringify(data));
+});
 
 app.listen(PORT, function () {
   console.log('App listening on PORT: ' + PORT);
